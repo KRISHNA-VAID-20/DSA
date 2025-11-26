72ccd95ab2930ae3f412fc19a5d65c73772a73f6
KRISHNA-VAID-20 <jrhixor@gmail.com>
2025-11-26 22:33:51 +0530

    Next permutation problem added

--- Array_Problems/Medium_array_problems.cpp
+++ Array_Problems/Medium_array_problems.cpp
@@ -313,7 +313,48 @@ int main(){
     }
 
     return 0;
-
-   
-
 }
+
+// Next permutation { better soln using inbuilt stl function}
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    vector <int> arr={3,2,1};
+
+    next_permutation(arr.begin(),arr.end());
+    for(auto it : arr){
+        cout << it;
+    }
+    return 0;
+}
+
+
+// (Better solution)  
+
+//   int n= nums.size();
+//         int index=-1;
+
+//         for(int i=n-2;i>=0;i--){
+//             if(nums[i]<nums[i+1]){
+//                 index=i;
+//                 break;
+//             }
+//         }
+//         if(index==-1){
+//             reverse(nums.begin(),nums.end());
+//             return ;
+//         }
+
+//         for(int i=n-1;i>index;i--){
+//             if(nums[i]>nums[index]){
+//                 swap(nums[i],nums[index]);
+//                 break;
+//             }
+
+//         }
+
+//         reverse(nums.begin()+ index+1,nums.end());
+//         return ;
