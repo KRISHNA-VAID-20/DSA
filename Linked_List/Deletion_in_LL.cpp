0e9fc9ad9a64457be9968c63268f515bfa0eb717
KRISHNA-VAID-20 <jrhixor@gmail.com>
2026-03-14 23:22:07 +0530

    Deletion in Linked List

---
+++ Linked_List/Deletion_in_LL.cpp
@@ -0,0 +1,169 @@
+// Deletion at front (head)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+
+Node* convert2LL(vector <int> &arr){
+    Node* head=new Node(arr[0]);
+    Node* mover=head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp=new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+
+}
+
+Node* deletehead(Node* head){
+    if(head==NULL) return head;
+
+    Node* temp=head;
+    head=head->next;
+    free(temp);
+    return head;
+}
+
+
+int main(){
+    vector <int> arr={1,2,3,4,5};
+    Node* head=convert2LL(arr);
+    head=deletehead(head);
+    // cout << head->data;
+    Node* temp=head;
+    while(temp){
+        cout << temp->data;
+        temp=temp->next;
+    }
+}
+
+// Deletion at end (tail)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+
+Node* convert2LL(vector <int> &arr){
+    Node* head=new Node(arr[0]);
+    Node* mover=head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp=new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+
+}
+
+Node* deleteatend(Node* head){
+    if(head==NULL || head->next==NULL) return NULL;
+    Node* temp=head;
+
+    while(temp->next->next!=NULL){
+        temp=temp->next;
+    }
+
+    free(temp->next);
+    temp->next=nullptr;
+
+    return head;
+    
+}
+
+
+int main(){
+    vector <int> arr={1,2,3,4,5};
+    Node* head=convert2LL(arr);
+    head=deleteatend(head);
+    Node* temp=head;
+    while(temp){
+        cout << temp->data << " ";
+        temp=temp->next;
+    }
+}
+
+// Deletion at particular point in Linked List 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+
+Node* convert2LL(vector <int> &arr){
+    Node* head=new Node(arr[0]);
+    Node* mover=head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp=new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+
+}
+
+Node* deletek(Node* head,int k){
+     if(head==NULL)return head;
+     
+     if(k==1){
+        Node* temp=head;
+        head=head->next;
+        free(temp);
+        return head;
+     }
+
+     int cnt=0;
+     Node* temp=head;
+     Node* prev=NULL;
+
+     while(temp!=NULL){
+        cnt++;
+        if(cnt==k){
+            prev->next=prev->next->next;
+            free(temp);
+            break;
+        }
+        prev=temp;
+        temp=temp->next;
+     }
+     return head;
+}
+
+
+int main(){
+    vector <int> arr={1,2,3,4,5};
+    Node* head=convert2LL(arr);
+    head=deletek(head,7);
+    Node* temp=head;
+    while(temp){
+        cout << temp->data << " ";
+        temp=temp->next;
+    }
+   
+}
