013d1afada6d51c31c346090f44a371b1d5d3f77
KRISHNA-VAID-20 <jrhixor@gmail.com>
2026-03-15 14:19:11 +0530

    Insertion operations in Singly Linked List

--- Linked_List/Insertion_in_LL.cpp
+++ Linked_List/Insertion_in_LL.cpp
@@ -1,7 +1,7 @@
 // // Insertion at front 
 
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
 // struct Node{
 //     int data;
@@ -111,3 +111,142 @@
 
 //     printLL(head);
 // }
+
+// Insertion at Postion 
+
+// struct Node{
+//     int data;
+//     Node* next;
+
+//     Node(int data1,Node* next1){
+//         data=data1;
+//         next=next1;
+//     }
+//     Node(int data1){
+//         data=data1;
+//         next=nullptr;
+//     }
+
+// };
+
+// Node* convert2LL(vector <int> &arr){
+//     Node* head=new Node(arr[0]);
+//     Node* mover=head;
+
+//     for(int i=1;i<arr.size();i++){
+//         Node* temp=new Node(arr[i]);
+//         mover->next=temp;
+//         mover=temp;
+//     }
+//     return head;
+// }
+
+// void printLL(Node* head){
+//     Node* temp=head;
+//     while(temp){
+//         cout << temp->data << " ";
+//         temp=temp->next;
+//     }
+// }
+
+// Node* insertatposition(Node* head,int val,int k){
+//     if(head==NULL){
+//         if(k==1){
+//             return new Node(val);
+//         }
+//         else{
+//             return head;
+//         }
+//     }
+
+//     if(k==1){
+//         return new Node(val,head);
+//         return head;
+//     }
+
+//     int cnt=0;
+//     Node* temp=head;
+
+//     while(temp!=NULL){
+//         cnt++;
+//         if(cnt==(k-1)){
+//             Node* newNode=new Node(val,temp->next);
+//             temp->next=newNode;
+
+//         }
+//         temp=temp->next;
+//     }
+//     return head;
+// }
+
+// int main(){
+//     vector <int> arr={1,2,3,4,5};
+//     Node* head=convert2LL(arr);
+//     head=insertatposition(head,100,2);
+
+//     printLL(head);
+// }
+
+struct Node{
+    int data;
+    Node* next;
+
+    Node(int data1,Node* next1){
+        data=data1;
+        next=next1;
+    }
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+
+};
+
+Node* convert2LL(vector <int> &arr){
+    Node* head=new Node(arr[0]);
+    Node* mover=head;
+
+    for(int i=1;i<arr.size();i++){
+        Node* temp=new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+
+void printLL(Node* head){
+    Node* temp=head;
+    while(temp){
+        cout << temp->data << " ";
+        temp=temp->next;
+    }
+}
+
+Node* insertbeforeValue(Node* head,int el,int val){
+    if(head==NULL){
+        return NULL;
+    }
+    if(head->data==val){
+        return new Node(el,head);
+    }
+    Node* temp=head;
+
+    while(temp->next!=NULL){
+        if(temp->next->data==val){
+            Node* newNode=new Node(el,temp->next);
+            temp->next=newNode;
+            break;
+
+        }
+        temp=temp->next;
+    }
+    return head;
+}
+
+int main(){
+    vector <int> arr={1,2,3,4,5};
+    Node* head=convert2LL(arr);
+    head=insertbeforeValue(head,100,6);
+
+    printLL(head);
+}
