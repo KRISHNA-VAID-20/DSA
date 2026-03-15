763c3131c324ce0df65c47e0512ad1d542350692
KRISHNA-VAID-20 <jrhixor@gmail.com>
2026-03-15 11:42:34 +0530

    Insertion in Linked List

--- Linked_List/Insertion_in_LL.cpp
+++ Linked_List/Insertion_in_LL.cpp
@@ -1,50 +1,113 @@
-#include <bits/stdc++.h>
-using namespace std;
+// // Insertion at front 
 
-struct Node{
-    int data;
-    Node* next;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-    Node(int data1,Node* next1){
-        data=data1;
-        next=next1;
-    }
-    Node(int data1){
-        data=data1;
-        next=nullptr;
-    }
+// struct Node{
+//     int data;
+//     Node* next;
 
-};
+//     Node(int data1,Node* next1){
+//         data=data1;
+//         next=next1;
+//     }
+//     Node(int data1){
+//         data=data1;
+//         next=nullptr;
+//     }
 
-Node* convert2LL(vector <int> &arr){
-    Node* head=new Node(arr[0]);
-    Node* mover=head;
+// };
 
-    for(int i=1;i<arr.size();i++){
-        Node* temp=new Node(arr[i]);
-        mover->next=temp;
-        mover=temp;
-    }
-    return head;
-}
+// Node* convert2LL(vector <int> &arr){
+//     Node* head=new Node(arr[0]);
+//     Node* mover=head;
 
-void printLL(Node* head){
-    Node* temp=head;
-    while(temp){
-        cout << temp->data << " ";
-        temp=temp->next;
-    }
-}
+//     for(int i=1;i<arr.size();i++){
+//         Node* temp=new Node(arr[i]);
+//         mover->next=temp;
+//         mover=temp;
+//     }
+//     return head;
+// }
 
-Node* insertatfront(int val,Node* head){
-    return new Node(val,head);
-}
+// void printLL(Node* head){
+//     Node* temp=head;
+//     while(temp){
+//         cout << temp->data << " ";
+//         temp=temp->next;
+//     }
+// }
 
-int main(){
-    vector <int> arr={1,2,3,4,5};
-    Node* head=convert2LL(arr);
-    head=insertatfront(100,head);
-    head=insertatfront(200,head);
+// Node* insertatfront(int val,Node* head){
+//     return new Node(val,head);
+// }
 
-    printLL(head);
-}
+// int main(){
+//     vector <int> arr={1,2,3,4,5};
+//     Node* head=convert2LL(arr);
+//     head=insertatfront(100,head);
+//     head=insertatfront(200,head);
+
+//     printLL(head);
+// }
+
+// // Insertion at End 
+
+
+// struct Node{
+//     int data;
+//     Node* next;
+
+//     Node(int data1,Node* next1){
+//         data=data1;
+//         next=next1;
+//     }
+//     Node(int data1){
+//         data=data1;
+//         next=nullptr;
+//     }
+
+// };
+
+// Node* convert2LL(vector <int> &arr){
+//     Node* head=new Node(arr[0]);
+//     Node* mover=head;
+
+//     for(int i=1;i<arr.size();i++){
+//         Node* temp=new Node(arr[i]);
+//         mover->next=temp;
+//         mover=temp;
+//     }
+//     return head;
+// }
+
+// void printLL(Node* head){
+//     Node* temp=head;
+//     while(temp){
+//         cout << temp->data << " ";
+//         temp=temp->next;
+//     }
+// }
+
+// Node* insertatend(int val,Node* head){
+//     if(head==NULL){
+//         return new Node(val);
+//     }
+
+//     Node* temp=head;
+//     while(temp->next!=NULL){
+//         temp=temp->next;
+//     }
+//     Node* newNode=new Node(val);
+//     temp->next=newNode;
+//     return head;
+
+// }
+
+// int main(){
+//     vector <int> arr={1,2,3,4,5};
+//     Node* head=convert2LL(arr);
+//     head=insertatend(100,head);
+
+//     printLL(head);
+// }
