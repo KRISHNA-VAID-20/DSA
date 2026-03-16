69cc0b2ad2e7711d819b041606d748cab353fd8c
KRISHNA-VAID-20 <jrhixor@gmail.com>
2026-03-16 23:47:15 +0530

    Deletion in Doubly Linked List

--- Linked_List/Deletion_in_Doubly_LL.cpp
+++ Linked_List/Deletion_in_Doubly_LL.cpp
@@ -1,5 +1,67 @@
+// Deletion of HEAD 
+
 #include <bits/stdc++.h>
 using namespace std;
+
+// struct Node{
+//     int data;
+//     Node* next;
+//     Node* back;
+
+//     Node(int data1,Node* next1,Node* back1){
+//         data=data1;
+//         next=next1;
+//         back=back1;
+//     }
+
+//     Node(int data1){
+//         data=data1;
+//         next=nullptr;
+//         back=nullptr;
+//     }
+// };
+
+// Node* Convert2DLL(vector <int> &arr){
+//     Node* head=new Node(arr[0]);
+//     Node* prev=head;
+
+//     for(int i=1;i<arr.size();i++){
+//         Node* temp=new Node(arr[i],nullptr,prev);
+//         prev->next=temp;
+//         prev=temp;
+//     }
+//     return head;
+// }
+
+// Node* Deletehead(Node* head){
+
+//     Node* prev=head;
+//     head = head->next;
+
+//     head->back=nullptr;
+//     prev->next=nullptr;
+
+//     delete prev;
+//     return head;
+
+// }
+
+// void printDLL(Node* head){
+//     while(head!=NULL){
+//         cout << head->data << " ";
+//         head=head->next;
+//     }
+// }
+
+// int main(){
+//     vector <int> arr={1,10,2,3,4,5};
+//     Node* head=Convert2DLL(arr);
+//     head=Deletehead(head);
+//     printDLL(head);
+//     return 0;
+// }
+
+// Deletion of TAIL 
 
 struct Node{
     int data;
@@ -31,17 +93,21 @@ Node* Convert2DLL(vector <int> &arr){
     return head;
 }
 
-Node* Deletehead(Node* head){
+Node* DeleteTail(Node* head){
+    if(head==NULL || head->next==NULL){
+        return NULL;
+    }
+    
+    Node* tail=head;
+    while(tail->next!=NULL){
+        tail=tail->next;
+    }
 
-    Node* prev=head;
-    head = head->next;
-
-    head->back=nullptr;
-    prev->next=nullptr;
-
-    delete prev;
+    Node* newtail=tail->back;
+    newtail->next=nullptr;
+    tail->back=nullptr;
+    delete tail;
     return head;
-
 }
 
 void printDLL(Node* head){
@@ -54,7 +120,7 @@ void printDLL(Node* head){
 int main(){
     vector <int> arr={1,10,2,3,4,5};
     Node* head=Convert2DLL(arr);
-    head=Deletehead(head);
+    head=DeleteTail(head);
     printDLL(head);
     return 0;
 }
