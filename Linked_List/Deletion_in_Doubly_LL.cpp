be81625c5511f26916f61fea8d5b5450dfeeef86
KRISHNA-VAID-20 <jrhixor@gmail.com>
2026-03-20 23:37:54 +0530

    Deletion at Kth Node in Doubly Linked List

--- Linked_List/Deletion_in_Doubly_LL.cpp
+++ Linked_List/Deletion_in_Doubly_LL.cpp
@@ -1,7 +1,67 @@
-// Deletion of HEAD 
+// // Deletion of HEAD 
 
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// // struct Node{
+// //     int data;
+// //     Node* next;
+// //     Node* back;
+
+// //     Node(int data1,Node* next1,Node* back1){
+// //         data=data1;
+// //         next=next1;
+// //         back=back1;
+// //     }
+
+// //     Node(int data1){
+// //         data=data1;
+// //         next=nullptr;
+// //         back=nullptr;
+// //     }
+// // };
+
+// // Node* Convert2DLL(vector <int> &arr){
+// //     Node* head=new Node(arr[0]);
+// //     Node* prev=head;
+
+// //     for(int i=1;i<arr.size();i++){
+// //         Node* temp=new Node(arr[i],nullptr,prev);
+// //         prev->next=temp;
+// //         prev=temp;
+// //     }
+// //     return head;
+// // }
+
+// // Node* Deletehead(Node* head){
+
+// //     Node* prev=head;
+// //     head = head->next;
+
+// //     head->back=nullptr;
+// //     prev->next=nullptr;
+
+// //     delete prev;
+// //     return head;
+
+// // }
+
+// // void printDLL(Node* head){
+// //     while(head!=NULL){
+// //         cout << head->data << " ";
+// //         head=head->next;
+// //     }
+// // }
+
+// // int main(){
+// //     vector <int> arr={1,10,2,3,4,5};
+// //     Node* head=Convert2DLL(arr);
+// //     head=Deletehead(head);
+// //     printDLL(head);
+// //     return 0;
+// // }
+
+// // Deletion of TAIL 
 
 // struct Node{
 //     int data;
@@ -33,17 +93,21 @@ using namespace std;
 //     return head;
 // }
 
-// Node* Deletehead(Node* head){
+// Node* DeleteTail(Node* head){
+//     if(head==NULL || head->next==NULL){
+//         return NULL;
+//     }
+    
+//     Node* tail=head;
+//     while(tail->next!=NULL){
+//         tail=tail->next;
+//     }
 
-//     Node* prev=head;
-//     head = head->next;
-
-//     head->back=nullptr;
-//     prev->next=nullptr;
-
-//     delete prev;
+//     Node* newtail=tail->back;
+//     newtail->next=nullptr;
+//     tail->back=nullptr;
+//     delete tail;
 //     return head;
-
 // }
 
 // void printDLL(Node* head){
@@ -56,12 +120,15 @@ using namespace std;
 // int main(){
 //     vector <int> arr={1,10,2,3,4,5};
 //     Node* head=Convert2DLL(arr);
-//     head=Deletehead(head);
+//     head=DeleteTail(head);
 //     printDLL(head);
 //     return 0;
 // }
 
-// Deletion of TAIL 
+// Deletion at kth place
+
+#include <bits/stdc++.h>
+using namespace std;
 
 struct Node{
     int data;
@@ -72,6 +139,7 @@ struct Node{
         data=data1;
         next=next1;
         back=back1;
+
     }
 
     Node(int data1){
@@ -81,7 +149,7 @@ struct Node{
     }
 };
 
-Node* Convert2DLL(vector <int> &arr){
+Node* convert2dll(vector<int> &arr){
     Node* head=new Node(arr[0]);
     Node* prev=head;
 
@@ -89,28 +157,72 @@ Node* Convert2DLL(vector <int> &arr){
         Node* temp=new Node(arr[i],nullptr,prev);
         prev->next=temp;
         prev=temp;
+
     }
     return head;
 }
 
-Node* DeleteTail(Node* head){
-    if(head==NULL || head->next==NULL){
-        return NULL;
-    }
-    
-    Node* tail=head;
-    while(tail->next!=NULL){
-        tail=tail->next;
+Node* deletehead(Node* head){
+    Node* temp=head;
+    head=head->next;
+    head->back=nullptr;
+    temp->next=nullptr;
+
+    delete temp;
+    return head;
+}
+
+Node* deletetail(Node* head){
+    Node* temp=head;
+
+    while(temp->next!=NULL){
+        temp=temp->next;
     }
 
-    Node* newtail=tail->back;
+    Node* newtail=temp->back;
     newtail->next=nullptr;
-    tail->back=nullptr;
-    delete tail;
+    temp->back=nullptr;
+    delete temp;
     return head;
+
 }
 
-void printDLL(Node* head){
+
+Node* deletekthpos(Node* head,int k){
+    
+    Node* temp=head;
+    int cnt=0;
+
+    while(temp!=NULL){
+
+        cnt++;
+        temp=temp->next;
+
+        if(cnt==k){
+
+        }
+        if(head==NULL || head->next==NULL){
+            return NULL;
+        }
+        
+        if(head->next && head->back == NULL){
+            return NULL;
+        }
+
+        if(head->back==NULL){
+            deletehead(head);
+        }        
+
+    }
+
+
+  
+
+
+}
+
+
+void printdll(Node* head){
     while(head!=NULL){
         cout << head->data << " ";
         head=head->next;
@@ -118,9 +230,7 @@ void printDLL(Node* head){
 }
 
 int main(){
-    vector <int> arr={1,10,2,3,4,5};
-    Node* head=Convert2DLL(arr);
-    head=DeleteTail(head);
-    printDLL(head);
-    return 0;
+    vector <int> arr={1,2,3,4,5};
+    Node* head=convert2dll(arr);
+    printdll(head);
 }
--- Linked_List/practice_sheet.cpp
+++ Linked_List/practice_sheet.cpp
@@ -1,3 +1,85 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// struct Node{
+//     int data;
+//     Node* next;
+//     Node* back;
+
+//     Node(int data1,Node* next1,Node* back1){
+//         data=data1;
+//         next=next1;
+//         back=back1;
+//     }
+
+//     Node(int data1){
+//         data=data1;
+//         next=nullptr;
+//         back=nullptr;
+//     }
+
+// };
+
+// Node* convert2dll(vector <int> &arr){
+//     Node* head=new Node(arr[0]);
+//     Node* prev=head;
+
+//     for(int i=1;i<arr.size();i++){
+//         Node* temp=new Node(arr[i],nullptr,prev);
+//         prev->next=temp;
+//         prev=temp;
+//     }
+//     return head;
+
+// }
+
+// // Delete head 
+
+// Node* deletehead(Node* head){
+//     Node* temp=head;
+//     head=head->next;
+//     head->back=nullptr;
+//     temp->next=nullptr;
+
+//     delete temp;
+//     return head;
+// }
+
+// // delete tail
+
+// Node* deletetail(Node* head){
+//     Node* temp=head;
+
+//     while(temp->next!=NULL){
+//         temp=temp->next;
+//     }
+
+//     Node* newtail=temp->back;
+//     newtail->next=nullptr;
+//     temp->back=nullptr;
+//     delete temp;
+//     return head;
+
+// }
+
+
+// void printdll(Node* head){
+//     while(head!=NULL){
+//         cout << head->data << " ";
+//         head=head->next;
+//     }
+// }
+
+
+// int main(){
+//     vector <int> arr={1,2,3,4,5};
+//     Node* head=convert2dll(arr);
+//     // head=deletehead(head);
+//     head=deletetail(head);
+//     printdll(head);
+
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -5,77 +87,114 @@ struct Node{
     int data;
     Node* next;
     Node* back;
-
+    
     Node(int data1,Node* next1,Node* back1){
         data=data1;
         next=next1;
         back=back1;
     }
-
+    
     Node(int data1){
         data=data1;
         next=nullptr;
         back=nullptr;
     }
-
 };
 
 Node* convert2dll(vector <int> &arr){
     Node* head=new Node(arr[0]);
     Node* prev=head;
-
+    
     for(int i=1;i<arr.size();i++){
         Node* temp=new Node(arr[i],nullptr,prev);
         prev->next=temp;
         prev=temp;
     }
     return head;
-
 }
 
-// Delete head 
+// delete head
 
-Node* deletehead(Node* head){
+Node* dhead(Node* head){
     Node* temp=head;
     head=head->next;
     head->back=nullptr;
-    temp->next=nullptr;
-
-    delete temp;
+    free(temp);
     return head;
 }
 
 // delete tail
 
-Node* deletetail(Node* head){
+Node* dtail(Node* head){
     Node* temp=head;
-
+    
     while(temp->next!=NULL){
         temp=temp->next;
     }
-
+    
     Node* newtail=temp->back;
     newtail->next=nullptr;
     temp->back=nullptr;
-    delete temp;
+    free (temp);
     return head;
+    
+}
 
+Node* datk(Node* head,int k){
+    if(head==NULL){
+        return NULL;
+    }
+    
+    int cnt=0;
+    Node* knode=head;
+    
+    while(knode!=NULL){
+        cnt++;
+        if(cnt==k) break;
+        knode=knode->next;
+        
+    }
+    
+    Node* prev=knode->back;
+    Node* front=knode->next;
+    
+    if(prev==NULL && front==NULL){
+        return NULL;
+    }
+    else if(prev==NULL){
+        return dhead(head);
+    }
+    else if(front==NULL){
+        return dtail(head);
+    }
+    
+    prev->next=front;
+    front->back=prev;
+    
+    knode->next=nullptr;
+    knode->back=nullptr;
+    free(knode);
+    return head;
+    
 }
 
 
 void printdll(Node* head){
+    
+    // Node* temp=head;
     while(head!=NULL){
         cout << head->data << " ";
         head=head->next;
     }
+
 }
 
 
 int main(){
     vector <int> arr={1,2,3,4,5};
     Node* head=convert2dll(arr);
-    // head=deletehead(head);
-    head=deletetail(head);
+    // head=dhead(head);
+    // head=dtail(head);
+    head=datk(head,2);
     printdll(head);
-
 }
