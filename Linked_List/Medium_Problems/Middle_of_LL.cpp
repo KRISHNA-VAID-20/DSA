2e98c7b65de4bd4e8c4dc0b8a21a63c94699451b
KRISHNA-VAID-20 <jrhixor@gmail.com>
2026-03-24 21:55:34 +0530

    TortiseHare Method (Middle Element of Linked List)

---
+++ Linked_List/Medium_Problems/Middle_of_LL.cpp
@@ -0,0 +1,61 @@
+// Time Complexity - O(N/2)
+// Space complexity = O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+
+    Node(int data1,Node* next1){
+        data=data1;
+        next=next1;
+    }
+
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+
+Node* convert2LL(vector <int> &arr){
+    Node* head=new Node(arr[0]);
+    Node* temp=head;
+
+    for(int i=1;i<arr.size();i++){
+        Node* newNode=new Node(arr[i]);
+        temp->next=newNode;
+        temp=newNode;
+
+    }
+    return head;
+}
+
+Node* findMiddle(Node* head){
+    Node* slow=head;
+    Node* fast=head;
+
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    return slow;
+}
+
+void printLL(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout << temp->data << " ";
+        temp=temp->next;
+    }
+}
+
+int main(){
+    vector <int> arr={1,2,3,4,5,6};
+    Node* head=convert2LL(arr);
+    printLL(head);
+    head=findMiddle(head);
+    cout << '\n' << head->data << endl;
+    return 0;
+}
