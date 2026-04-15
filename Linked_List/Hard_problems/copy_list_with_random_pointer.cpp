13e38ffd076502547a0464ca403d1a42d0a7c3cb
KRISHNA-VAID-20 <jrhixor@gmail.com>
2026-04-15 23:54:19 +0530

    Copy list with random pointer

---
+++ Linked_List/Hard_problems/copy_list_with_random_pointer.cpp
@@ -0,0 +1,54 @@
+// /*
+// // Definition for a Node.
+// class Node {
+// public:
+//     int val;
+//     Node* next;
+//     Node* random;
+    
+//     Node(int _val) {
+//         val = _val;
+//         next = NULL;
+//         random = NULL;
+//     }
+// };
+// */
+
+// class Solution {
+// public:
+//     Node* copyRandomList(Node* head) {
+//         if (!head) return NULL;
+
+//         Node* curr = head;
+//         while (curr) {
+//             Node* newNode = new Node(curr->val);
+//             newNode->next = curr->next;
+//             curr->next = newNode;
+//             curr = newNode->next;
+//         }
+
+//         curr = head;
+//         while (curr) {
+//             if (curr->random) {
+//                 curr->next->random = curr->random->next;
+//             }
+//             curr = curr->next->next;
+//         }
+
+//         curr = head;
+//         Node* newHead = head->next;
+        
+//         while (curr) {
+//             Node* copy = curr->next;
+//             curr->next = copy->next;
+            
+//             if (copy->next) {
+//                 copy->next = copy->next->next;
+//             }
+            
+//             curr = curr->next;
+//         }
+
+//         return newHead;
+//     }
+// };
